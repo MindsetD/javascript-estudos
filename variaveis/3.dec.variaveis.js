26143954c7673ab369d5d9ec799b9ff3d81e42dd
Danilo <mindsetcodeaspire01@gmail.com>
2026-02-25 01:24:06 -0300

    declarando variaveis

---
+++ variaveis/3.dec.variaveis.js
@@ -0,0 +1,18 @@
+var nome = "robert" 
+// variavel tipo string 
+
+const emailpessoa = "danilosilva@gmail.com"
+
+
+nomepessoa = "arquimedes"
+
+
+
+
+let telefonepessoa = "83939878"
+
+console.log(telefonepessoa)
+
+
+// se pode criar variaveis com letras maiúsculas e minusculas 
+// não se pode criar variaveis com espaço. 
