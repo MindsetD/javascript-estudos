f8b03fa1501ad4ff51879217b960a91ad7139fd9
Danilo <mindsetcodeaspire01@gmail.com>
2026-03-17 15:30:15 -0300

    tipos primitivos no javascript

---
+++ console-e-comentarios/tipos.primitivos/8.primitivos.js
@@ -0,0 +1,21 @@
+// Tipos primitivos 
+
+// numero 
+// string 
+// booleano 
+// indefinido // existe, mas não recebeu valor ainda
+// nulo // null → valor vazio definido pelo programador
+
+let texto = ("soudamamae")
+let numero = (50)
+let booleano = true // ou false
+let nulo = null
+let indefinido = undefined
+
+// esses são os tipos primitivos que aprendemos hoje 
+// o nulo é nulo, o indefinido é quando algo que ainda não existe; booleano é se uma sentença é falsa ou verdadeira
+// e sem aspas são numeros, com aspas são strings (texto)
+
+/* Tipo	Quem define	Significado
+undefined	JavaScript	ainda não tem valor
+null	Programador	vazio intencional */
